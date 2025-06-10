f103a22e7eefaf88d15f6ccbd768dd7981d9188c
Sunil Nayak <sunilnyk2345@gmail.com>
2025-06-10 19:23:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ function GfGWeatherApp() {
         <source src={'https://cdn.pixabay.com/video/2022/07/04/123091-726838254_large.mp4'} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <img src="https://th.bing.com/th/id/R.770b805d5c99c7931366c2e84e88f251?rik=khgO%2bY1Hh3BT9w&riu=http%3a%2f%2fpurepng.com%2fpublic%2fuploads%2flarge%2fpurepng.com-weather-iconsymbolsiconsapple-iosiosios-8-iconsios-8-721522596142qx4ep.png&ehk=6msbAydV7X6D4bO8zvLC664aXwKOdBU17dwrHcKxaAg%3d&risl=&pid=ImgRaw&r=0" alt="" className=' weather-icon'/>
+      <img src="https://static.vecteezy.com/system/resources/thumbnails/009/700/808/small/cloud-rain-icon-eps-10-free-vector.jpg" alt="" className=' weather-icon'/>
             <h1 className="app-name">
                  Weather App
             </h1>
